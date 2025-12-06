e7b45eb9cbef184fe60cc1fbf0e2b30bc03a7755
neelgit1010 <neel.binbrain1010@gmail.com>
2025-12-06 13:32:38 +0530

    update

--- src/controllers/variable.controller.ts
+++ src/controllers/variable.controller.ts
@@ -25,10 +25,13 @@ export const getVariables = async (req: Request, res: Response) => {
   try {
     const page = Number(req.query.page) || 1;
     const limit = req.query.limit ? Number(req.query.limit) : 10;
+      const sort = req.query.sort ? String(req.query.sort) : "desc";
+      const sortOrder = sort === "asc" ? 1 : -1;
 
     const total = await Variable.countDocuments();
 
     const variables = await Variable.find()
+      .sort({ createdAt: sortOrder })
       .skip((page - 1) * limit)
       .limit(limit)
       .lean<VariableDoc[]>();
